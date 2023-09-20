9cfb59e5e1b6162073a829a23bfa1cae2516d26f
José Pedretti Neto <jpedretti@gmail.com>
2023-09-20 16:43:13 -0400

    removes unused tag

--- Assets/Editor/Enum Tags/Scripts/Tags.cs
+++ Assets/Editor/Enum Tags/Scripts/Tags.cs
@@ -7,5 +7,4 @@ public const string T_EditorOnly = @"EditorOnly";
 public const string T_MainCamera = @"MainCamera";
 public const string T_Player = @"Player";
 public const string T_GameController = @"GameController";
-public const string T_Anchor = @"Anchor";
 }
