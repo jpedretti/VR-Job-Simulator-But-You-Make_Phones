cc38cbcba656439d1cd271c8714110ed13839868
José Pedretti Neto <jpedretti@gmail.com>
2023-09-23 21:19:45 -0400

    Adds a collider to the torch flame and controls it with the flame. FIxes bug when SelectExit without Deactivate.

--- Assets/Scripts/Torch.cs
+++ Assets/Scripts/Torch.cs
@@ -3,17 +3,28 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 namespace com.NW84P
 {
+    [Tooltip("The Torch class represents a grabbable torch object in a VR environment with a fire particle effect and a capsule collider for interaction.")]
     public class Torch : XRGrabInteractable
     {
+
+        private const float _ParticleLifeTime = 0.15f;
+        private const float _FireColliderHeight = 0.017f;
+
         private ParticleSystem _fireParticle;
         private XRBaseController _controller;
         private ParticleSystem.MainModule _mainParticlaModule;
+        private CapsuleCollider _fireCollider;
+        private Vector3 _fireColliderCenter;
 
         public void Start()
         {
             _fireParticle = GetComponentInChildren<ParticleSystem>();
             _mainParticlaModule = _fireParticle.main;
+            _fireCollider = _fireParticle.GetComponent<CapsuleCollider>();
             _mainParticlaModule.startLifetime = 0;
+            _fireCollider.enabled = false;
+            _fireColliderCenter = new Vector3(_fireCollider.center.x, _fireCollider.center.y, 0);
+            _fireCollider.center = _fireColliderCenter;
         }
 
         protected override void OnEnable()
@@ -21,12 +32,14 @@ namespace com.NW84P
             base.OnEnable();
             activated.AddListener(Activate);
             deactivated.AddListener(Deactivate);
+            selectExited.AddListener(SelectExited);
         }
 
         protected override void OnDisable()
         {
             activated.RemoveListener(Activate);
             deactivated.RemoveListener(Deactivate);
+            selectExited.RemoveListener(SelectExited);
             base.OnDisable();
         }
 
@@ -37,7 +50,11 @@ namespace com.NW84P
             if (_controller != null && updatePhase == XRInteractionUpdateOrder.UpdatePhase.Dynamic)
             {
                 var triggerValue = _controller.activateInteractionState.value;
-                _mainParticlaModule.startLifetime = triggerValue * 0.2f;
+                _mainParticlaModule.startLifetime = triggerValue * _ParticleLifeTime;
+                _fireCollider.height = triggerValue * _FireColliderHeight;
+                _fireCollider.enabled = _fireCollider.height >= _FireColliderHeight / 2;
+                _fireColliderCenter.z = _fireCollider.height;
+                _fireCollider.center = _fireColliderCenter;
             }
         }
 
@@ -50,11 +67,19 @@ namespace com.NW84P
             }
         }
 
-        private void Deactivate(DeactivateEventArgs args)
+        private void Deactivate(DeactivateEventArgs args) => ResetState();
+
+        private void SelectExited(SelectExitEventArgs args) => ResetState();
+
+        private void ResetState()
         {
             _fireParticle.Stop();
             _mainParticlaModule.startLifetime = 0;
             _controller = null;
+            _fireCollider.enabled = false;
+            _fireCollider.height = 0;
+            _fireColliderCenter.z = 0;
+            _fireCollider.center = _fireColliderCenter;
         }
 
 #if UNITY_EDITOR
@@ -64,6 +89,11 @@ namespace com.NW84P
             if (GetComponentInChildren<ParticleSystem>() == null)
             {
                 Debug.LogError($"No Particle System found in {gameObject}");
+            }
+
+            if (GetComponentInChildren<CapsuleCollider>() == null)
+            {
+                Debug.LogError($"No Capsule Collider found in {gameObject}");
             }
         }
 
