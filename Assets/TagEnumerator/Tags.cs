702de96e6774bf49d2ffba9b0fd7dce2e1bdda6f
José Pedretti Neto <jpedretti@gmail.com>
2023-09-25 20:51:53 -0400

    Moves TagEnumerator

--- Assets/TagEnumerator/Tags.cs
+++ Assets/TagEnumerator/Tags.cs
@@ -7,7 +7,5 @@ public const string EditorOnly = @"EditorOnly";
 public const string MainCamera = @"MainCamera";
 public const string Player = @"Player";
 public const string GameController = @"GameController";
-public const string WeldPath = @"WeldPath";
-public const string Anchor = @"Anchor";
 public const string TorchFire = @"TorchFire";
 }
