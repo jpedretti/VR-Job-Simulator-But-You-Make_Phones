89caf36ec08e1ab0dd582c0217a0ce4070ae2636
José Pedretti Neto <jpedretti@gmail.com>
2023-09-29 23:08:59 -0400

    Improves Screw code

--- Assets/Scripts/Screw.cs
+++ Assets/Scripts/Screw.cs
@@ -14,10 +14,13 @@ namespace com.NW84P
         [SerializeField]
         private Transform _screwSocketTransform;
 
+        private const float _SCREW_MOVE_DISTANCE = 0.00003f;
+        private const float _SCREW_DISTANCE = 0.006F;
+
         #region Screw
 
         private Transform _screwTransform;
-        private Vector3 _screwPreviousPosition;
+        private Vector3 _screwInitialPosition;
         private Vector3 _vector3RotationAxis;
 
         #endregion Screw
@@ -43,20 +46,31 @@ namespace com.NW84P
         private bool _isValidTriggerEnter;
         private bool _canSnap;
 
+        private bool IsScrewPositionBelowInitial
+            => _rotationAxis.GetAxisValue(_screwTransform.position) < _rotationAxis.GetAxisValue(_screwInitialPosition);
+
+        private bool IsScrewPositionExceedingMaxDistance
+            => _rotationAxis.GetAxisValue(_screwTransform.position) - _rotationAxis.GetAxisValue(_screwInitialPosition)
+                > _SCREW_DISTANCE;
+
         private void Start()
         {
             _screwTransform = transform.parent;
             _vector3RotationAxis = _rotationAxis.ToVector3Axis();
-            _screwPreviousPosition = _screwTransform.position;
+            _screwInitialPosition = _screwTransform.position;
             _isValidTriggerEnter = true;
         }
+
+        public void OnDestroy() => ResetState();
+
+        public void OnDisable() => ResetState();
 
         public void OnTriggerEnter(Collider other)
         {
             if (other.gameObject.CompareTag(Tags.ScrewDriver) && _isValidTriggerEnter)
             {
                 _screwdriverTransform = other.gameObject.transform;
-                if (SetXRComponents())
+                if (InitializeXRComponents())
                 {
                     _canSnap = true;
                     _isValidTriggerEnter = false;
@@ -66,7 +80,7 @@ namespace com.NW84P
             }
         }
 
-        private bool SetXRComponents()
+        private bool InitializeXRComponents()
         {
             var result = false;
             if (_screwdriverTransform.gameObject.TryGetComponent<XRGrabInteractable>(out var interactable))
@@ -77,6 +91,7 @@ namespace com.NW84P
                     _handTransform = interactable.interactorsSelecting[0].transform;
                     _handOriginalRotation = _handTransform.rotation.eulerAngles.z;
                     interactable.selectExited.AddListener(SelectionEnded);
+                    interactable.activated.AddListener(ActivationStarted);
                     result = true;
                 }
                 else
@@ -124,6 +139,7 @@ namespace com.NW84P
                 _screwdriverInteractable.trackPosition = true;
                 _screwdriverInteractable.trackRotation = true;
                 _screwdriverInteractable.selectExited.RemoveListener(SelectionEnded);
+                _screwdriverInteractable.activated.RemoveListener(ActivationStarted);
                 _screwdriverInteractable = null;
             }
         }
@@ -137,6 +153,8 @@ namespace com.NW84P
                     _isSnapped = true;
                     _screwdriverInteractable.trackPosition = false;
                     _screwdriverInteractable.trackRotation = false;
+                    RotateScrewdriverWithHand();
+                    _screwdriverPreviousRotation = _screwdriverTransform.rotation;
                 }
 
                 if (_isSnapped && (!_canSnap || Vector3.Distance(_screwdriverTransform.position, _handTransform.position) > 0.15f))
@@ -148,39 +166,63 @@ namespace com.NW84P
 
                 if (_isSnapped)
                 {
-                    _screwdriverTransform.position = _screwSocketTransform.position - _distance;
-
-                    var sourceEuler = _screwSocketTransform.rotation.eulerAngles;
-                    var originalRotationAxisRotation = _handOriginalRotation + _handTransform.rotation.eulerAngles.z;
-                    var newRotation = Quaternion.Euler(sourceEuler.x, sourceEuler.y, originalRotationAxisRotation);
-                    _screwdriverTransform.rotation = newRotation;
-
+                    RotateScrewdriverWithHand();
                     PerformScrew();
+                    _screwdriverTransform.position = _screwSocketTransform.position - _distance;
                 }
             }
+        }
+
+        private void RotateScrewdriverWithHand()
+        {
+            var sourceEuler = _screwSocketTransform.rotation.eulerAngles;
+            var originalRotationAxisRotation = _handOriginalRotation + _handTransform.rotation.eulerAngles.z;
+            var newRotation = Quaternion.Euler(sourceEuler.x, sourceEuler.y, originalRotationAxisRotation);
+            _screwdriverTransform.rotation = newRotation;
         }
 
         private bool IsAtRightPosition()
         {
             var interactableLocalUp = _screwdriverAttachTransform.InverseTransformDirection(_vector3RotationAxis);
             var attachLocalUp = _screwSocketTransform.InverseTransformDirection(_vector3RotationAxis);
-            return Vector3.Dot(interactableLocalUp, attachLocalUp) >= 0.9f;
+            return Vector3.Dot(interactableLocalUp, attachLocalUp) >= 0.85f;
         }
 
         private void PerformScrew()
         {
-            var currentRotation = _screwdriverTransform.rotation;
-            var rotationDelta = currentRotation * Quaternion.Inverse(_screwdriverPreviousRotation);
+            if (!IsScrewPositionBelowInitial && !IsScrewPositionExceedingMaxDistance)
+            {
+                var currentRotation = _screwdriverTransform.rotation;
+                var rotationDelta = currentRotation * Quaternion.Inverse(_screwdriverPreviousRotation);
 
-            rotationDelta.ToAngleAxis(out float angle, out Vector3 axis);
+                rotationDelta.ToAngleAxis(out float angle, out Vector3 axis);
 
-            var side = Vector3.Dot(axis, _vector3RotationAxis) < 0 ? 1 : -1;
-            float screwRotation = angle * _rotationMultiplier * side;
-            _screwTransform.Rotate(_vector3RotationAxis, screwRotation);
+                // Normalize the angle to be within the range of 0 to 360 degrees
+                angle = (angle %= 360) > 180 ? angle - 360 : angle;
 
-            _screwdriverPreviousRotation = currentRotation;
+                var side = Vector3.Dot(axis, _vector3RotationAxis) < 0 ? 1 : -1;
+
+                float screwRotation = angle * _rotationMultiplier * side;
+                _screwTransform.Rotate(_vector3RotationAxis, screwRotation);
+
+                float moveDistance = screwRotation * _SCREW_MOVE_DISTANCE;
+                _screwTransform.position += _vector3RotationAxis * moveDistance;
+
+                _screwdriverPreviousRotation = currentRotation;
+            }
+
+            if (IsScrewPositionBelowInitial)
+            {
+                _screwTransform.position = _screwInitialPosition;
+            }
+            else if (IsScrewPositionExceedingMaxDistance)
+            {
+                _screwTransform.position = _screwInitialPosition + _vector3RotationAxis * _SCREW_DISTANCE;
+            }
         }
 
         private void SelectionEnded(SelectExitEventArgs args) => _canSnap = false;
+
+        private void ActivationStarted(ActivateEventArgs args) => _canSnap = false;
     }
 }
