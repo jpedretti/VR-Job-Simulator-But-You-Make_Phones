daea397ddc7a2ef360357afb23329fc10ca3442b
José Pedretti Neto <jpedretti@gmail.com>
2023-10-04 18:39:32 -0400

    Fixes a bug where the custom socket would select the interactable even if it wasn't snapped.

--- Assets/Scripts/CustomSocket.cs
+++ Assets/Scripts/CustomSocket.cs
@@ -5,12 +5,31 @@ namespace com.NW84P
 {
     public class CustomSocket : XRSocketInteractor
     {
+        [Header("Custom Configurations")]
+        [Tooltip("Only allow selection of the XRGrabInteractable object if it is snapped.")]
+        [SerializeField]
+        private bool _onlySelectIfSnaped;
+
         private bool _isSnapped;
         private IXRSelectInteractor _hand;
         private XRGrabInteractable _interactable;
 
         private const float _SNAPPING_ALIGNMENT_THRESHOLD = 0.994f;
         private const float _UNSNAPPING_DISTANCE_THRESHOLD = 0.8f;
+
+        /// <summary>
+        /// Returns true if the XRGrabInteractable object is snapped, false otherwise.
+        /// </summary>
+        public bool IsSnapped => _isSnapped;
+
+        /// <summary>
+        /// Gets or sets the value indicating whether only allow selection of the XRGrabInteractable object if it is snapped.
+        /// </summary>
+        public bool OnlySelectIfSnapped
+        {
+            get => _onlySelectIfSnaped;
+            set => _onlySelectIfSnaped = value;
+        }
 
         protected override void OnEnable()
         {
@@ -41,6 +60,9 @@ namespace com.NW84P
             }
         }
 
+        public override bool CanSelect(IXRSelectInteractable interactable)
+            => base.CanSelect(interactable) && (!_onlySelectIfSnaped || _isSnapped);
+
         private void EndSnap(Transform interactableTransform)
         {
             if (_isSnapped
@@ -62,11 +84,11 @@ namespace com.NW84P
 
         private bool IsAtRightPosition(Transform interactableTransform)
         {
-            var interactableLocalUp = interactableTransform.InverseTransformDirection(Vector3.up);
-            var attachLocalUp = attachTransform.InverseTransformDirection(Vector3.up);
+            var interactableLocalUp = interactableTransform.up;
+            var attachLocalUp = attachTransform.up;
 
-            var interactableLocalForward = interactableTransform.InverseTransformDirection(Vector3.forward);
-            var attachLocalForward = attachTransform.InverseTransformDirection(Vector3.forward);
+            var interactableLocalForward = interactableTransform.forward;
+            var attachLocalForward = attachTransform.forward;
 
             var rightUp = Vector3.Dot(interactableLocalUp, attachLocalUp) >= _SNAPPING_ALIGNMENT_THRESHOLD;
             var rightForward = Vector3.Dot(interactableLocalForward, attachLocalForward) >= _SNAPPING_ALIGNMENT_THRESHOLD;
