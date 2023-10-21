b96dcff497e38fb3fa279336bf8551b14df5bb97
José Pedretti Neto <jpedretti@gmail.com>
2023-10-20 21:03:15 -0400

    Adds haptic feedback when moving the handwheel

--- Assets/Scripts/PressMachine/HandwheelHandle.cs
+++ Assets/Scripts/PressMachine/HandwheelHandle.cs
@@ -5,12 +5,15 @@ namespace com.NW84P
 {
     public class HandwheelHandle : XRBaseInteractable
     {
+        private const float _MAX_TORQUE = 0.13f;
+
         [Header("Handwheel Handle Settings")]
         [SerializeField]
         private Handwheel _handwheel;
 
         private Transform _handTransform;
         private Transform _handleTransform;
+        private XRBaseController _handController;
         private Vector3 _selectionPoint;
         private Vector3 _previousHandlePosition;
 
@@ -38,11 +41,22 @@ namespace com.NW84P
                 var direction = _handTransform.position - _selectionPoint;
                 var magnitude = direction.magnitude;
                 var dot = Vector3.Dot(direction.normalized, _handleTransform.forward.normalized);
-                var torque = dot * magnitude;
+                var torque = Mathf.Clamp(dot * magnitude, -_MAX_TORQUE, _MAX_TORQUE);
 
                 _handwheel.ApplyTorque(torque);
+
+                SendHapticFeedback(torque);
                 _selectionPoint += _handleTransform.position - _previousHandlePosition;
                 _previousHandlePosition = _handleTransform.position;
+            }
+        }
+
+        private void SendHapticFeedback(float torque)
+        {
+            if (_handController != null)
+            {
+                var amplitude = Mathf.Abs(torque) / _MAX_TORQUE;
+                _handController.SendHapticImpulse(amplitude, 0.1f);
             }
         }
 
@@ -51,6 +65,7 @@ namespace com.NW84P
             if (args.interactorObject.transform.gameObject.CompareTag(Tags.Player))
             {
                 _handTransform = args.interactorObject.transform;
+                _handController = args.interactorObject.GetController();
                 _selectionPoint = _handTransform.position;
                 _previousHandlePosition = _handleTransform.position;
             }
